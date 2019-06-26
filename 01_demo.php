ca1e5d78e60d3f31860af0168c985fc91a6150ec
cheyanlin <che1420275103@163.com>
2019-06-26 16:25:54 +0800

    完成了登录的类的声明；添加了相应的属性和方法 ......

---
+++ 01_demo.php
@@ -0,0 +1,85 @@
+<?php
+
+$int1 = 100;
+
+echo $Int1;
+
+echo '中华人民共和国建国70周年万岁';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style type="text/css">
+		*{
+			color: red;
+		}
+	</style>
+</head>
+<body>
+	<a href="http://www.baidu.com">百度</a>
+	<span onclick="window.alert('我是弹出内容')">上海</span>
+	<span onclick="style.color='blue'">上海</span>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
